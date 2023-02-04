39a88c9b4fc3454f49e85dd454f3630d798ea9d6
GeorgeMC2610 <55163372+GeorgeMC2610@users.noreply.github.com>
2023-02-04 02:07:05 +0200

    added objects and obstacles in GeorgeMC2610 scene

--- Assets/Scripts/FPSController.cs
+++ Assets/Scripts/FPSController.cs
@@ -8,7 +8,7 @@ public class FPSController : MonoBehaviour
 {
     public float walkingSpeed = 7.5f;
     public float runningSpeed = 11.5f;
-    public float jumpSpeed = 8.0f;
+    public float jumpSpeed = 0f;
     public float gravity = 20.0f;
     public Camera playerCamera;
     public float lookSpeed = 2.0f;
