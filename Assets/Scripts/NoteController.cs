cb68fcf6849dc62f490f712c8323f0a2ab3fa5d3
GeorgeMC2610 <giorgosseimenis@gmail.com>
2023-02-04 13:40:04 +0200

    Moved the text a little bit down
    Also made the if-statement for the Note Controller a bit more understandable

--- Assets/Scripts/NoteController.cs
+++ Assets/Scripts/NoteController.cs
@@ -12,7 +12,7 @@ public class NoteController : MonoBehaviour
     public TextMeshProUGUI Text;
     public bool LookingAtNote = false;
     public bool HasOpenedNote = false;
-    
+
     void Start()
     {
         
@@ -22,25 +22,34 @@ public class NoteController : MonoBehaviour
     {
         // dray a Ray. Looking for "Note" tags with it.
         FireRay();
-        
+
         // looking at a note is when the ray hits a GameObject with a tag of "Note".
         LookingAtNote = (hitData.collider != null && hitData.collider.CompareTag("Note"));
 
         // Enable/Disable the text that informs the player about being able to read a note.
         Text.enabled = LookingAtNote;
 
-        if (Input.GetKey(KeyCode.F) && LookingAtNote && !HasOpenedNote)
+        // if the player presses the F Key while looking at a note...
+        if (Input.GetKey(KeyCode.F) && LookingAtNote)
         {
-            Debug.Log("You Opened the Note.");
-            HasOpenedNote = true;
-        }
-        else if (Input.GetKey(KeyCode.F) && LookingAtNote && HasOpenedNote)
-        {
-            Debug.Log("You Closed the Note.");
-            HasOpenedNote = false;
+            // if the player hasn't opened the note yet, open it.
+            if (!HasOpenedNote)
+            {
+                Debug.Log("You Opened the Note.");
+                HasOpenedNote = true;
+                // TODO: Add code that disables player movement.
+            }
+            
+            // otherwise, close it.
+            else
+            {
+                Debug.Log("You Closed the Note.");
+                HasOpenedNote = false;
+                // TODO: Add code that re-enables player movement.
+            }
         }
     }
-    
+
     /// <summary>
     /// Draws a <see cref="Ray"/> relative to the player camera. Whatever it hits gets saved
     /// in the <see cref="hitData"/> variable.
