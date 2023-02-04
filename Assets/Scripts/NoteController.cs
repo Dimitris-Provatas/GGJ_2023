b93ad0bc0a8318fbadf740a9b515c3757728ac4e
GeorgeMC2610 <55163372+GeorgeMC2610@users.noreply.github.com>
2023-02-04 13:02:26 +0200

    Added "Note" tag
    Also checking for the Note tag in the controller

--- Assets/Scripts/NoteController.cs
+++ Assets/Scripts/NoteController.cs
@@ -1,11 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class NoteController : MonoBehaviour
 {
     private Ray ray;
     private RaycastHit hitData;
+    
+    public TextMeshPro Text;
+    public bool LookingAtNote = false;
     
     void Start()
     {
@@ -16,9 +20,19 @@ public class NoteController : MonoBehaviour
     {
         FireRay();
         
-        if (hitData.collider != null && hitData.collider.name.StartsWith("Note"))
+        if (hitData.collider != null && hitData.collider.CompareTag("Note"))
         {
             Debug.Log("Found a note!");
+            LookingAtNote = true;
+        }
+        else
+        {
+            LookingAtNote = false;
+        }
+
+        if (LookingAtNote)
+        {
+            
         }
     }
     
